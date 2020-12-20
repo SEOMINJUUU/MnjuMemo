8c53400bbe81e58a290b05e00fe917f6785ab5ea
서민주 <minju1307@gmail.com>
2020-12-21 00:09:30 +0900

    Initial Commit

---
+++ MnjuMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MnjuMemo
+//
+//  Created by 서민주 on 2020/12/21.
+//  Copyright © 2020 Minju Seo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
