7c58799054e9e7e36e70f8753169a8e4bded3c41
서민주 <minju1307@gmail.com>
2020-12-22 20:33:07 +0900

    새 메모 화면

---
+++ MnjuMemo/ComposeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ComposeViewController.swift
+//  MnjuMemo
+//
+//  Created by 서민주 on 2020/12/22.
+//  Copyright © 2020 Minju Seo. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
