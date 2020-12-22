da28d9ea12929c877a16a0d4571c1fd8239d1baa
서민주 <minju1307@gmail.com>
2020-12-23 00:17:27 +0900

    데이터베이스에 저장

--- MnjuMemo/Data/DataManager.swift
+++ MnjuMemo/Data/DataManager.swift
@@ -32,7 +32,16 @@ class DataManager { // singleton
         } catch  {
             print(error)
         }
+
+    }
+    
+    func addNewMemo(_ memo: String?) {
+        let newMemo = Memo(context: mainContext)
+        newMemo.content = memo
+        newMemo.insertDate = Date()
         
+        memoList.insert(newMemo, at: 0)
+        saveContext()
     }
     
     // MARK: - Core Data stack
